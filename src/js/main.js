3507d2e307db307f3781ca15f4d0b8db1aa6b569
Ilya <lord-ilja@yandex.ru>
2022-06-14 15:17:19 +0300

    add Wow animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import './modules/slider';
+import WOW from './modules/wow';
 import { modalsModule } from './modules/modals';
 import tabsModule from './modules/tabs';
 import formsModule from './modules/forms';
@@ -7,17 +8,17 @@ import timerModule from './modules/timer';
 import imagesModule from './modules/images';
 
 window.addEventListener('DOMContentLoaded', () => {
-  'use strict';
-
   const modalState = {
     form: 0,
     type: 'tree',
   };
   changeModalStateModule(modalState);
 
-  modalsModule('show', 3000, modalState);
+  modalsModule('show', 60000, modalState);
   tabsModule();
   formsModule(modalState);
   timerModule('#timer', '2022-10-01');
   imagesModule();
+
+  new WOW().init();
 });
